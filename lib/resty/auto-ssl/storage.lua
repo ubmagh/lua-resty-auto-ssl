cf9592766e6ad1d11a1dabfebc601b1ff3c932d2
ayoub <ubmagh@gmail.com>
2026-08-09 22:02:17 +0100

    Update storage.lua

--- lib/resty/auto-ssl/storage.lua
+++ lib/resty/auto-ssl/storage.lua
@@ -107,7 +107,7 @@ function _M.get_certs_for_renewal(self, expiry_threshold, enable_redis_sorted_li
   local keys, err
   if type(self.adapter.keys_with_suffix_under_expiry_threashold) == "function" and enable_redis_sorted_list_renewal then
     keys, err = self.adapter:keys_with_suffix_under_expiry_threashold( expiry_threshold)
-  else 
+  else
     keys, err = self.adapter:keys_with_suffix(":latest")
   end
   if err then
